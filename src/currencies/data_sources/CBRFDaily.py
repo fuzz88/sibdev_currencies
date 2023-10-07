d8493492186a43e9936268688e7a589f1d9541e6
fuzz88 <ivan@oschepkov.ru>
2023-10-07 15:42:22 +0800

    refactor and comment CBRFDaily

--- src/currencies/data_sources/CBRFDaily.py
+++ src/currencies/data_sources/CBRFDaily.py
@@ -8,11 +8,18 @@ from .base import DataSource
 
 
 class CBRFDaily(DataSource):
+    RETRY_COUNT = 5
+
     @staticmethod
     async def fetch_archive(date: tuple) -> dict:
+        """
+        Идем за данными запросом HTTP GET.
+        В случае нескольких неудачных попыток возвращаем объект с описанием ошибки.
+        Все исключения транспорта обрабатываются внутри этой корутины.
+        """
         year, month, day = date
 
-        async def fetch(retry_count=3):
+        async def fetch(retry_count=CBRFDaily.RETRY_COUNT):
             try:
                 url = f"https://www.cbr-xml-daily.ru/archive/{year}/{month:02}/{day:02}/daily_json.js"
                 async with httpx.AsyncClient() as client:
@@ -21,7 +28,9 @@ class CBRFDaily(DataSource):
                         raise Exception(f"Retry. {resp.status_code} {url}")
                     return resp.json()
             except Exception as e:
-                await asyncio.sleep((3 - retry_count) * 1.0)  # progressive delay
+                await asyncio.sleep(
+                    (CBRFDaily.RETRY_COUNT + 1 - retry_count) * 1.0
+                )  # progressive delay
                 if retry_count == 0:
                     return {"error": resp.status_code}
                 return await fetch(retry_count - 1)
@@ -31,7 +40,8 @@ class CBRFDaily(DataSource):
     @staticmethod
     def get_updates(dates: Set[date]) -> list[dict]:
         """
-        asyncroniously fetch archive for past 30 days and return results when all ready.
+        Закидываем список дат, для которых хотим получить данные, в корутины.
+        Корутины асинхронно получают данные и обрабатывают ошибки сами.
         """
         fetching_tasks = asyncio.gather(
             *list(
