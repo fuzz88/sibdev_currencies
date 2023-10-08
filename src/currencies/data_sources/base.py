f7c28195c5fabc0644a582d83699a520fc0bd9ec
fuzz88 <ivan@oschepkov.ru>
2023-10-08 21:18:27 +0800

    being precise

--- src/currencies/data_sources/base.py
+++ src/currencies/data_sources/base.py
@@ -1,8 +1,10 @@
 from abc import ABCMeta, abstractmethod
+from datetime import date
+from typing import Set
 
 
 class DataSource(metaclass=ABCMeta):
     @staticmethod
     @abstractmethod
-    def get_updates():
+    def get_updates(dates: Set[date]) -> list[dict]:
         pass
