420be48537d75b7b228cf4a2c8e1394e1f42a1b4
fuzz88 <ivan@oschepkov.ru>
2023-10-11 11:24:40 +0800

    add permissions

--- src/app/settings.py
+++ src/app/settings.py
@@ -139,9 +139,6 @@ AUTH_USER_MODEL = "users.User"
 REST_FRAMEWORK = {
     # Use Django's standard `django.contrib.auth` permissions,
     # or allow read-only access for unauthenticated users.
-    "DEFAULT_PERMISSION_CLASSES": [
-        "rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly"
-    ],
     "DEFAULT_AUTHENTICATION_CLASSES": (
         "rest_framework_simplejwt.authentication.JWTAuthentication",
     ),
--- src/currencies/views.py
+++ src/currencies/views.py
@@ -1,5 +1,6 @@
 from rest_framework import viewsets
 from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 
 from currencies.models import CurrencyRate, CurrencyThreshold
 from currencies.serializers import CurrencyRateSerializer, CurrencyThresholdSerializer
@@ -8,14 +9,19 @@ from currencies.serializers import CurrencyRateSerializer, CurrencyThresholdSeri
 class CurrencyRateViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = CurrencyRate.objects.all()
     serializer_class = CurrencyRateSerializer
+    permission_classes = IsAuthenticatedOrReadOnly
 
     def list(self, request, *args, **kwargs):
         if not request.user.is_authenticated:
+            # для неавторизированного пользователя стандарный рид-онли ответ
             return super().list(request, *args, **kwargs)
         else:
+            # для авторизированного делаем джоин с таблицей пороговых значений
+            # TODO
             return Response(data=[])
 
 
 class CurrencyThresholdViewSet(viewsets.ModelViewSet):
     queryset = CurrencyThreshold.objects.all()
     serializer_class = CurrencyThresholdSerializer
+    permission_classes = IsAuthenticated
