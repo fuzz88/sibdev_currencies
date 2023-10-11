a35173e9716b4a0c73099cd2dc4242d9cf987701
fuzz88 <ivan@oschepkov.ru>
2023-10-11 10:34:09 +0800

    add CurrencyThreshold model

--- src/currencies/models.py
+++ src/currencies/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 
+from users.models import User
+
 
 class CurrencyRate(models.Model):
     cbrf_id = models.CharField(max_length=10)
@@ -10,3 +12,9 @@ class CurrencyRate(models.Model):
     value = models.DecimalField(max_digits=12, decimal_places=4)
     previous_value = models.DecimalField(max_digits=12, decimal_places=4)
     date = models.DateField()
+
+
+class CurrencyThreshold(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    char_code = models.CharField(max_length=3)
+    threshold_value = models.DecimalField(max_digits=12, decimal_places=4)
