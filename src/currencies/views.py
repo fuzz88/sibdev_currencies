6c4fd36b058dd66413423a42a39c027ce3d194c2
fuzz88 <ivan@oschepkov.ru>
2023-10-11 10:33:57 +0800

    CurrencyRateViewSet now differs for auth and non-auth user

--- src/currencies/views.py
+++ src/currencies/views.py
@@ -1,14 +1,21 @@
 from rest_framework import serializers, viewsets
-
+from rest_framework.response import Response
 from currencies.models import CurrencyRate
 
 
 class CurrencyRateSerializer(serializers.ModelSerializer):
+    charcode = serializers.CharField(source="char_code")
     class Meta:
         model = CurrencyRate
-        fields = ["id", "date", "char_code", "value"]
+        fields = ["id", "date", "charcode", "value"]
 
 
-class CurrencyRateViewSet(viewsets.ModelViewSet):
+class CurrencyRateViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = CurrencyRate.objects.all()
     serializer_class = CurrencyRateSerializer
+
+    def list(self, request, *args, **kwargs):
+        if not request.user.is_authenticated:
+            return super().list(request, *args, **kwargs)
+        else:
+            return Response(data=[])
